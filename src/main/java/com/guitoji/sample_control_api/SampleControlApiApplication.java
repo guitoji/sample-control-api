8abdcf614d1b239da42399d764c5e756e2a86b7e
guitoji <guilherme.limaal22@outlook.com>
2026-04-25 18:59:48 -0300

    Project files created

---
+++ src/main/java/com/guitoji/sample_control_api/SampleControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.guitoji.sample_control_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guitoji/sample_control_api/SampleControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guitoji.sample_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
